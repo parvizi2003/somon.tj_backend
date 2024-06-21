99e1e62b4bf8c077b96c2fb7c278e65a90980afd
Parviz Haydarov <parvizi2003@gmail.com>
2024-06-21 17:09:39 +0500

    migration fixed

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -32,7 +32,7 @@ const config = {
 
 export default config;
 
-// // In this file you can configure migrate-mongo
+// In this file you can configure migrate-mongo
 // const config = {
 //   mongodb: {
 //     // TODO Change (or review) the url to your MongoDB:
@@ -42,8 +42,8 @@ export default config;
 //     databaseName: "somon",
 
 //     options: {
-//       useNewUrlParser: true, // removes a deprecation warning when connecting
-//       useUnifiedTopology: true, // removes a deprecating warning when connecting
+//       // useNewUrlParser: true, // removes a deprecation warning when connecting
+//       // useUnifiedTopology: true, // removes a deprecating warning when connecting
 //       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
 //       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
 //     }
--- migrations/20240619111613-update-todo-collection.js
+++ migrations/20240619111613-update-todo-collection.js
@@ -1,7 +1,12 @@
+import PostModel from "../models/Post"
+import UserModel from "../models/User"
+
 export async function up(db, client) {
-  await db.createCollection('users')
-}
+  await db.createCollection('users', UserModel)
+  await db.createCollection('posts', PostModel)
+};
   
 export async function down(db, client) {
   await db.collection('users').drop()
-}
+  await db.collection('posts').drop()
+};
